f4022b5848a3f012cf1441f55fb91933df53625a
joelll-u <120340981+joelll-u@users.noreply.github.com>
2022-12-12 20:04:02 +0000

    solution for day 8 part 2

--- day-8/solution.py
+++ day-8/solution.py
@@ -43,5 +43,38 @@ for i in visibility_matrix:
     sum_ += sum(i)
 print((width * height) - sum_)
 
+scale_array = [[1] * (width - 2) for i in range(height - 2)]
 
+def get_most_recent(val, recent, dist):
+    most = max(recent[val:])
+    return dist - most
 
+for i in range(1, height - 1):
+    recent_left = [0] * 10
+    recent_right = [0] * 10
+
+    for ptr in range(1, width - 1):
+        # from left
+        tree_l = int(forest[i][ptr])
+        scale_array[i - 1][ptr - 1] *= get_most_recent(tree_l, recent_left, ptr)
+        recent_left[tree_l] = ptr
+
+        tree_r = int(forest[i][width - ptr - 1])
+        scale_array[i - 1][width - ptr - 2] *= get_most_recent(tree_r, recent_right, ptr)
+        recent_right[tree_r]  = ptr
+
+for i in range(1, width - 1):
+    recent_upper = [0] * 10
+    recent_lower = [0] * 10
+
+    for ptr in range(1, height - 1):
+        # from left
+        tree_u = int(forest[ptr][i])
+        scale_array[ptr - 1][i - 1] *= get_most_recent(tree_u, recent_upper, ptr)
+        recent_upper[tree_u] = ptr
+
+        tree_l = int(forest[height - ptr - 1][i])
+        scale_array[height - ptr - 2][i - 1] *= get_most_recent(tree_l, recent_lower, ptr)
+        recent_lower[tree_l] = ptr
+
+print(max([max(a) for a in scale_array]))

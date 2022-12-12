3e48f5dc10500542e4082a511250eeea6e599fcd
joelll-u <120340981+joelll-u@users.noreply.github.com>
2022-12-12 00:56:35 +0000

    Solution to day 1 part 2

--- day-1/solution1.py
+++ day-1/solution1.py
@@ -1,3 +1,5 @@
+# Part 1
+
 f = open("input1.txt", "r")
 curr_max = 0
 curr_sum = 0
@@ -9,3 +11,24 @@ for line in f:
     else:
         curr_sum += int(line.rstrip())
 print(curr_max)
+
+# Part 2
+f.seek(0)
+top3 = [0, 0, 0]
+
+def add_to_top3(num):
+    for i in range(3):
+        if num > top3[i]:
+            top3.insert(i, num)
+            top3.pop()
+            break
+
+curr_sum = 0
+for line in f:
+    if line == "\n":
+        add_to_top3(curr_sum)
+        curr_sum = 0
+    else:
+        curr_sum += int(line.rstrip())
+print(sum(top3))
+f.close()

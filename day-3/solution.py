dde9b3607036eb937cfcf338989d02d6f441222e
joelll-u <120340981+joelll-u@users.noreply.github.com>
2022-12-12 01:56:45 +0000

    solution to day 3 part 2

--- day-3/solution.py
+++ day-3/solution.py
@@ -22,3 +22,26 @@ for line in f:
     priority_sum += get_priority(find_overlap(c1, c2))
 
 print(priority_sum)
+
+# part 2
+f.seek(0)
+
+def find_triple_overlap(str1, str2, str3):
+    str1s = set(str1)
+    str2s = set(str2)
+    for i in str3:
+        if i in str1s and i in str2s:
+            return i
+    print(str1, str2, str3)
+
+curr_group = []
+badge_sum = 0
+
+for line in f:
+    curr_group.append(line.rstrip())
+    if len(curr_group) == 3:
+        o = find_triple_overlap(curr_group[0], curr_group[1], curr_group[2])
+        badge_sum += get_priority(o)
+        curr_group = []
+
+print(badge_sum)

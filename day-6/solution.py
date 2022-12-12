8db4ede401a686d0cc4b6e23f02bba9b98b4ff62
joelll-u <120340981+joelll-u@users.noreply.github.com>
2022-12-12 14:03:03 +0000

    solution to day 6 part 2

--- day-6/solution.py
+++ day-6/solution.py
@@ -3,19 +3,22 @@ f = open("input.txt", "r")
 line = f.read().rstrip()
 
 #part 1
-seen = set()
-left = 0
-right = 0
-while right < len(line):
-    if not line[right] in seen:
-        if right - left == 3:
-            print(right + 1)
-            break
-        seen.add(line[right])
-        right += 1
-    else:
-        while line[left] != line[right]:
-            seen.remove(line[left])
+def find_distinct_char(line, n):
+    seen = set()
+    left = 0
+    right = 0
+    while right < len(line):
+        if not line[right] in seen:
+            if right - left == (n - 1):
+                return right + 1
+            seen.add(line[right])
+            right += 1
+        else:
+            while line[left] != line[right]:
+                seen.remove(line[left])
+                left += 1
             left += 1
-        left += 1
-        right += 1
+            right += 1
+
+print(find_distinct_char(line, 4))
+print(find_distinct_char(line, 14))
